0174d4281541cd142e7c8f5735dd84a051ca0f83
Eana Hufwe <ilove@1a23.com>
2018-04-16 23:09:58 +1000

    Add menus to the nav bar

--- public/javascripts/mcw.js
+++ public/javascripts/mcw.js
@@ -11,3 +11,18 @@ mdc.autoInit();
 [].forEach.call(document.querySelectorAll('.mdc-list-item'), function (surface) {
     mdc.ripple.MDCRipple.attachTo(surface);
 });
+
+[].forEach.call(document.querySelectorAll('.mdc-menu'), function (menuDom) {
+    var menu = new mdc.menu.MDCMenu(menuDom);
+    document.querySelector(menuDom.dataset.anchor).addEventListener('click', function () {
+        menu.open = !menu.open;
+    });
+    if (menuDom.dataset.anchorPosition) {
+        menu.setAnchorCorner(mdc.menu.MDCMenuFoundation.Corner[menuDom.dataset.anchorPosition]);
+    }
+});
+
+
+/* Navbar only */
+let drawer = new mdc.drawer.MDCTemporaryDrawer(document.querySelector('.mdc-drawer--temporary'));
+document.querySelector('.navbar-drawer-toggle').addEventListener('click', () => drawer.open = true);
