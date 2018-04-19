ef3546f9872461df7b0cff9ea0d2e5960f3633d0
phamnhattuong <iamnhvt@gmail.com>
2018-04-19 16:09:29 +1000

    Updating authentication middleware

---
+++ controller/authenticationMiddleware.js
@@ -0,0 +1,6 @@
+function authenticateUser(req, res, next) {
+    res.locals.user = req.user;
+    next();
+}
+
+module.exports = authenticateUser;
--- routes/startups.js
+++ routes/startups.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.locals.user = req.user;
     res.render('startups/landing', { title: 'Startups — Raison' });
 });
 
