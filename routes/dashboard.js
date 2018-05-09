cce33262660c789998f00dc876643863ed91a861
Eana Hufwe <ilove@1a23.com>
2018-05-09 16:56:50 +1000

    Thread list details and Project edit

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -11,6 +11,14 @@ router.get('/inbox', function (req, res, next) {
     res.render('dashboard/inbox', { title: 'Inbox (0) — Raison' });
 });
 
+router.get('/inbox/new', function (req, res, next) {
+    res.render('dashboard/thread-new', { title: 'New thread — Raison' });
+});
+
+router.get('/inbox/:id', function (req, res, next) {
+    res.render('dashboard/thread-detail', { title: 'Queries regarding your project Lorem Ipsum — Raison' });
+});
+
 router.get('/profile', function (req, res, next) {
     res.render('dashboard/profile', { title: 'Profile — Raison' });
 });
@@ -18,6 +26,14 @@ router.get('/profile', function (req, res, next) {
 router.get('/projects', function (req, res, next) {
     res.locals.projects = db.getProjectUser(res.locals.user.username);
     res.render('dashboard/projects', { title: 'Projects — Raison' });
+});
+
+router.get('/projects/new', function (req, res, next) {
+    res.render('dashboard/projects-edit', { title: 'New Projects — Raison' });
+});
+
+router.get('/projects/:id', function (req, res, next) {
+    res.render('dashboard/projects-edit', { title: 'Edit — Lorem Ipsum Innovation Project — Raison' });
 });
 
 router.get('/security', function (req, res, next) {
